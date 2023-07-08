3babac5717a02fb41ad9d863f401ee43ebad6d64
mihajlo <122311433+oljahim@users.noreply.github.com>
2023-07-08 02:51:16 +0200

    NumberOfEvents added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import "./App.css";
+import NumberOfEvents from "./components/NumberOfEvents";
 
 const App = () => {
   return (
     <div className="App">
       <CitySearch />
       <EventList />
+      <NumberOfEvents />
     </div>
   );
 };
