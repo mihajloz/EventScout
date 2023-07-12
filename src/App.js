a96ddc34ff3fa7bcb5ab97de7944593742a19695
mihajlo <122311433+oljahim@users.noreply.github.com>
2023-07-12 23:25:34 +0200

    integrated google api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React, { useState, useEffect } from "react";
 import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
-import "./App.css";
 import NumberOfEvents from "./components/NumberOfEvents";
-import { getEvents } from "./api";
+import { useEffect, useState } from "react";
+import { extractLocations, getEvents } from "./api";
+
+import "./App.css";
 
 const App = () => {
   const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE] = useState(32);
-
-  const fetchData = async () => {
-    const allEvents = await getEvents();
-    setEvents(allEvents.slice(0, currentNOE));
-  };
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [currentCity]);
 
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents =
+      currentCity === "See all cities"
+        ? allEvents
+        : allEvents.filter((event) => event.location === currentCity);
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  };
   return (
     <div className="App">
-      <CitySearch />
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
       <NumberOfEvents />
       <EventList events={events} />
     </div>
